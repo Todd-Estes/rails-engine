80adec4a0e3f5c22e83189cd38a9b4683c086138
todd-estes <elestes@gmail.com>
2020-12-15 19:38:30 -0700

    Update serializers; added most_revenue endpoint method to merchant model; WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+# before running "rake db:seed", do the following:
+# - put the "rails-engine-development.pgdump" file in db/data/
+# - put the "items.csv" file in db/data/
+
+cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails-engine_development db/data/rails-engine-development.pgdump"
+puts "Loading PostgreSQL Data dump into local database with command:"
+puts cmd
+system(cmd)
+
+file = "./db/data/items.csv"
+CSV.foreach(file, headers: true) do |row|
+  data = row.to_hash
+  data["unit_price"] = data["unit_price"].to_f/100.round(2)
+  Item.create(data)
+end
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
