dcd96e4adefade82d6af1d5c7c903053c83bb442
todd-estes <elestes@gmail.com>
2020-12-16 18:34:12 -0700

    Made changes to request and related record test for merchants and items after serializers added

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -10,15 +10,23 @@ describe "Merchants API" do
 
     merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants.count).to eq (3)
+    expect(merchants).to have_key(:data)
 
-    merchants.each do |merchant|
+    merchants[:data].each do |merchant|
+      expect(merchant).to have_key(:attributes)
+    end
+
+    expect(merchants[:data].count).to eq (3)
+
+    merchants[:data].each do |merchant|
+
+
 
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_a(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_an(String)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 
@@ -31,11 +39,11 @@ describe "Merchants API" do
 
     expect(response).to be_successful
 
-    expect(merchant).to have_key(:id)
-    expect(merchant[:id]).to eq(id)
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id].to_i).to eq(id)
 
-    expect(merchant).to have_key(:name)
-    expect(merchant[:name]).to be_a(String)
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
 
   it "can create a new merchant" do
